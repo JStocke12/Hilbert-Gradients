3d9a90b3ddb48cb7133031f7eeccacff0c9781d0
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-05-25 15:56:12 -0400

    Finished Hilbert Curve Recursive Logic

--- draw.py
+++ draw.py
@@ -1,6 +1,13 @@
-Hilbert_Array = [[[0],[1]],[[3],[2]]]
+Hilbert_Array = [[0]]
+
+def bit_prepend(h, n):
+    s = h[-1][0].bit_length()
+    return [[(n << s) | e for e in l] for l in h]
 
 def expand(h):
-    return list(zip(h,h))+list(zip(h,h))
+    return [sum(i, []) for i in list(zip(list(map(list, zip(*h))),bit_prepend(h,1)))+list(zip(list(map(lambda l: list(l)[::-1], zip(*bit_prepend(h,3))))[::-1],bit_prepend(h,2)))]
 
-print(expand(Hilbert_Array))
+for i in range(4):
+    Hilbert_Array = expand(Hilbert_Array)
+
+print(Hilbert_Array)
