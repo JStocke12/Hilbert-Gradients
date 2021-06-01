67016b346ae20edf97f6baa1b74a8a0ccd940e9f
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-06-01 16:09:11 -0400

    Added image logic

--- draw.py
+++ draw.py
@@ -1,3 +1,8 @@
+from PIL import Image
+from numpy import array
+from numpy import vectorize
+from colorsys import hsv_to_rgb as hsv
+
 Hilbert_Array = [[0]]
 
 def bit_prepend(h, n):
@@ -7,7 +12,15 @@ def bit_prepend(h, n):
 def expand(h):
     return [sum(i, []) for i in list(zip(list(map(list, zip(*h))),bit_prepend(h,1)))+list(zip(list(map(lambda l: list(l)[::-1], zip(*bit_prepend(h,3))))[::-1],bit_prepend(h,2)))]
 
-for i in range(4):
+for i in range(10):
     Hilbert_Array = expand(Hilbert_Array)
 
-print(Hilbert_Array)
+img = Image.new('HSV', (len(Hilbert_Array),len(Hilbert_Array)))
+
+px = img.load()
+
+for i in range(len(Hilbert_Array)):
+    for j in range(len(Hilbert_Array)):
+        px[i,j] = (Hilbert_Array[i][j]%256, 255, 255)
+
+img.show()
